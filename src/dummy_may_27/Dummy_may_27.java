26cbd6f0e44efec808285b584b8b8e80433cd35e
ronak sheth <ronak.sheth@sheridancollege.ca>
2022-05-27 13:42:04 -0400

    Revert "added comment"
    This reverts commit 4554801ebf0ffbceab7af3c4759f425cb5ed4318.

--- src/dummy_may_27/Dummy_may_27.java
+++ src/dummy_may_27/Dummy_may_27.java
@@ -6,7 +6,6 @@ public class Dummy_may_27 {
     public static void main(String[] args) {
         // TODO code application logic here
         //comments added at 1:37PM
-        //comments added at 1:40PM
     }
     
 }
